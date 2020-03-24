a08d1aaf32d0431506cd1f35f43aace316734618
Pauline Tolila <pauline.tolila@gmail.com>
2020-03-24 19:18:05 +0100

    route espions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :words, :games
-  get 'games/:id/espions', to: 'games#espions'
+  resources :games
+  get 'games/:id/espions', to: 'games#espions', as: :espions
 end
